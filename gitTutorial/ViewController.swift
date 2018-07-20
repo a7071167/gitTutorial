738537935f92ec62c0972f146fbf80940e8f8efb
Alexander Kufel <kufelalexander@gmail.com>
2018-07-20 14:07:32 +0300

    Initial Commit

---
+++ gitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTutorial
+//
+//  Created by user on 20.07.2018.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
